02c41875d8a7a471ec1c163721eadd117758d7da
Darlene-13 <darlenenasimiyu@gmail.com>
2025-10-27 15:00:11 +0300

    Feat: Encoding and Decoding exception

--- src/main/java/io/github/darlene/exception/DecodingException.java
+++ src/main/java/io/github/darlene/exception/DecodingException.java
@@ -1,7 +1,11 @@
 package io.github.darlene.exception;
 
 public class DecodingException extends RuntimeException {
-    public DecodingException(String message) {
-        super(message);
+
+    public DecodingException(String name){
+        super("Decoding error occurred in file" + name);
+    }
+    public DecodingException(String name, Throwable cause){
+        super("Decoding error occurred in file: " + name, cause);
     }
 }
--- src/main/java/io/github/darlene/exception/EncodingException.java
+++ src/main/java/io/github/darlene/exception/EncodingException.java
@@ -1,7 +1,10 @@
 package io.github.darlene.exception;
 
 public class EncodingException extends RuntimeException {
-    public EncodingException(String message) {
-        super(message);
+    public EncodingException(String name){
+        super("Encoding error occurred in file: " + name);
+    }
+    public EncodingException(String name, Throwable cause){
+        super("Encoding error occurred in file: " + name, cause);
     }
 }
