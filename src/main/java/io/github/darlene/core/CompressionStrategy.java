ad9e7df9b7ff8a48440db01ff5b50428774c013b
Darlene-13 <darlenenasimiyu@gmail.com>
2025-10-27 14:28:59 +0300

    Defined compression strategies every algorithm should implement

--- src/main/java/io/github/darlene/core/CompressionStrategy.java
+++ src/main/java/io/github/darlene/core/CompressionStrategy.java
@@ -1,16 +1,55 @@
 package io.github.darlene.core;
 
+
+// Interface to define the compression strategy each algorithm must implement
+// Interface methods do not have a body as they have to be implemented by the classes that implement this interface
 public interface CompressionStrategy {
 
+    // Define method to compress
+    public default void compressFile(String sourceFilePath, String destinationFilePath, int compressionLevel){
+        // Default implementation (can be overridden by implementing classes)
+    }
 
+    // Method to return success status
+    public default void getCompressionSuccessStatus(){
+    }
 
+    // Method to return compression
+    public static double getCompressionRatio(){
+        return 0.0;
+    }
 
+    // Method to return time taken for compression
+    public static long getCompressionTime(){
+        return 0L;
+    }
 
+    // Method to return original file size
+    public static long getOriginalFileSize(){
+        return 0L;
+    }
 
+    // Method to return compressed file size
+    public static long getCompressedFileSize(){
+        return 0L;
+    }
 
+    //Method to display the compression details: Time it took, original size, compressed size,
+    public static void displayCompressionDetails(){
+        // Default implementation (can be overridden by implementing classes)
+    }
 
+    // Define method to decompress
+    public default void decompressFile(String sourceFilePath, String destinationFilePath){
+        // Default implementation (can be overridden by implementing classes)
+    }
 
+    // Decompression success status
+    public default void getDecompressionSuccessStatus(){
+    }
 
+    // Method to display the decompression details
+    public default void displayDecompressionDetails(){
 
-
+    }
 }
