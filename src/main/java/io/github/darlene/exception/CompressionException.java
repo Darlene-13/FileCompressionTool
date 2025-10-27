6f77011c34c6279d8019ded3cfea9c9330ebe9db
Darlene-13 <darlenenasimiyu@gmail.com>
2025-10-27 14:50:15 +0300

    Feat: File not found exception extending runtime exception

--- src/main/java/io/github/darlene/exception/CompressionException.java
+++ src/main/java/io/github/darlene/exception/CompressionException.java
@@ -1,7 +1,5 @@
 package io.github.darlene.exception;
 
 public class CompressionException extends RuntimeException {
-    public CompressionException(String message) {
-        super(message);
-    }
+
 }
--- src/main/java/io/github/darlene/exception/FileAccessException.java
+++ src/main/java/io/github/darlene/exception/FileAccessException.java
@@ -1,7 +1,5 @@
 package io.github.darlene.exception;
 
 public class FileAccessException extends RuntimeException {
-    public FileAccessException(String message) {
-        super(message);
-    }
+
 }
