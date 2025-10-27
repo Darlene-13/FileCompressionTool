c93a1fcd47badc5aa31af85c1296d4fd627a7118
Darlene-13 <darlenenasimiyu@gmail.com>
2025-10-27 13:52:00 +0300

    Feat:Added default compression level param

--- src/main/java/io/github/darlene/core/CompressionFormat.java
+++ src/main/java/io/github/darlene/core/CompressionFormat.java
@@ -5,29 +5,35 @@ import java.util.Set;
 
 public enum CompressionFormat {
 
-    ZIP(Set.of(CompressionType.DEFLATE), ".zip"),
-    TAR(Set.of(), ".tar"),   // Archive format without compression
-    GZIP(Set.of(CompressionType.DEFLATE), ".gz"),
-    TAR_GZ(Set.of(CompressionType.DEFLATE), ".tar.gz"),
-    BZIP2(Set.of(CompressionType.BWT, CompressionType.MTF, CompressionType.HUFFMAN), ".bz2"),
-    XZ(Set.of(CompressionType.LZMA2), ".xz"),
-    RAR(Set.of(CompressionType.PPM, CompressionType.LZSS, CompressionType.RANGE), ".rar");
+    ZIP(Set.of(CompressionType.DEFLATE), ".zip", 9),
+    TAR(Set.of(), ".tar", 9),   // Archive format without compression
+    GZIP(Set.of(CompressionType.DEFLATE), ".gz", 9),
+    TAR_GZ(Set.of(CompressionType.DEFLATE), ".tar.gz", 9),
+    BZIP2(Set.of(CompressionType.BWT, CompressionType.MTF, CompressionType.HUFFMAN), ".bz2", 9),
+    XZ(Set.of(CompressionType.LZMA2), ".xz", 9),
+    RAR(Set.of(CompressionType.PPM, CompressionType.LZSS, CompressionType.RANGE), ".rar", 9);
 
 
     private final Set<CompressionType> algorithms;
     private final String fileExtension;
+    private final int defaultCompressionLevel;
 
 
 
     // Constructors and methods
-    CompressionFormat (Set<CompressionType> algorithms, String fileExtension){
+    CompressionFormat (Set<CompressionType> algorithms, String fileExtension, int defaultCompressionLevel){
         this.algorithms = algorithms;
         this.fileExtension = fileExtension;
+        this.defaultCompressionLevel = defaultCompressionLevel;
     }
 
     // Getter
     public Set<CompressionType> getAlgorithms(){
         return algorithms;
+    }
+
+    public int getDefaultCompressionLevel() {
+        return defaultCompressionLevel;
     }
 
     //Method to find file format based on file extension
@@ -39,5 +45,4 @@ public enum CompressionFormat {
         }
         return null;
     }
-
 }
