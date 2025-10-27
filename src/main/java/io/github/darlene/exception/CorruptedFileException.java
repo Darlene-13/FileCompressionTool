4714eff4bc73c9507d3b9655877e4d0a1ac73a3c
Darlene-13 <darlenenasimiyu@gmail.com>
2025-10-27 14:54:43 +0300

    Corrupted file exception

--- src/main/java/io/github/darlene/exception/CorruptedFileException.java
+++ src/main/java/io/github/darlene/exception/CorruptedFileException.java
@@ -1,7 +1,13 @@
 package io.github.darlene.exception;
 
 public class CorruptedFileException extends RuntimeException {
-    public CorruptedFileException(String message) {
-        super(message);
+
+
+    public CorruptedFileException(String name){
+        super("The file is corrupted: " + name);
+
+    }
+    public CorruptedFileException(String name, Throwable cause){
+        super("The file is corrupted: " + name, cause);
     }
 }
