b29ff5f36e85c23ea5752ede3f59879317dccc6a
Darlene-13 <darlenenasimiyu@gmail.com>
2025-10-28 11:15:23 +0300

    Feat:Frequency analyzer with hashmap to create a frequency table

--- src/main/java/io/github/darlene/algorithms/huffman/FrequencyAnalyzer.java
+++ src/main/java/io/github/darlene/algorithms/huffman/FrequencyAnalyzer.java
@@ -1,4 +1,39 @@
+
 package io.github.darlene.algorithms.huffman;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
+// Calculates and analyzes how many times each byte appears and returns map of byte and frequency.
 public class FrequencyAnalyzer {
+
+    Map<Byte, Integer> frequencyTable;
+
+    // Constructor
+    public FrequencyAnalyzer(byte[] data){
+        this.frequencyTable = new HashMap<>();
+        analyze(data);
+    }
+
+    // Method to analyze
+    public void analyze(byte[] data){
+        // Check if it is null
+
+        // Counts the byte
+        for (byte b: data){
+            if (frequencyTable.containsKey(b)){
+                frequencyTable.put(b, frequencyTable.get(b) +1);
+            } else{
+                frequencyTable.put(b, 1 );
+            }
+        }
+    }
+
+    // Populate the map
+    public Map<Byte, Integer> getFrequencyTable(Map<Byte, Integer> frequencyTable){
+        return frequencyTable;
+    }
 }
+
+
