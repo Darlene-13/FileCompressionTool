b0d3e9659caf2e5a654125b49b49ba1144657fce
Darlene-13 <darlenenasimiyu@gmail.com>
2025-10-28 05:06:45 +0300

    Huffman Encoder, constructor and attributes

--- src/main/java/io/github/darlene/algorithms/huffman/HuffmanEncoder.java
+++ src/main/java/io/github/darlene/algorithms/huffman/HuffmanEncoder.java
@@ -6,6 +6,32 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class HuffmanEncoder {
-
+    private HuffmanNode root;
     Map<Byte, String> codingTable;
+
+    // Constructor
+    public HuffmanEncoder(HuffmanTree){
+        this.root = HuffmanTree.getRoot();
+        this.codingTable = new HashMap<>();
+        buildCodingTable(this.root, "");
+    }
+
+    //Generate codes method
+    public void generateCodes(){
+
+
+
+
+    }
+
+    public void getCodes(){
+        // Return the coding table
+
+
+    }
+
+    public void buildCodingTable(HuffmanNode node, String code){
+
+    }
+
 }
