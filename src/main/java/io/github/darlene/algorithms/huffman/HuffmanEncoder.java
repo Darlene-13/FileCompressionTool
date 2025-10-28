d3a367c8c967d469920319b878d80d3f865901cc
Darlene-13 <darlenenasimiyu@gmail.com>
2025-10-28 09:23:54 +0300

    Method to generate code, buildcoding table

--- src/main/java/io/github/darlene/algorithms/huffman/HuffmanEncoder.java
+++ src/main/java/io/github/darlene/algorithms/huffman/HuffmanEncoder.java
@@ -1,7 +1,5 @@
 package io.github.darlene.algorithms.huffman;
 
-import io.github.darlene.algorithms.huffman.HuffmanTree;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,27 +8,42 @@ public class HuffmanEncoder {
     Map<Byte, String> codingTable;
 
     // Constructor
-    public HuffmanEncoder(HuffmanTree){
-        this.root = HuffmanTree.getRoot();
+    public HuffmanEncoder(HuffmanTree tree){
+        this.root = tree.getRoot();
         this.codingTable = new HashMap<>();
-        buildCodingTable(this.root, "");
+        generateCodes();
     }
 
     //Generate codes method
     public void generateCodes(){
-
-
-
-
-    }
-
-    public void getCodes(){
-        // Return the coding table
-
-
+        buildCodingTable(this.root,"");
     }
 
     public void buildCodingTable(HuffmanNode node, String code){
+        // Edge case 1: If node is null
+        if (node == null){
+            return;
+        }
+        // Edge case 2: If node is a leaf node
+        if (node.isLeaf()){
+            codingTable.put(node.getSymbol(), code);
+            return;
+        }
+        // Recursive calls for left and right child nodes
+        buildCodingTable(node.getLeft(), code + "0");
+        buildCodingTable(node.getRight(), code + "1");
+
+
+    }
+    // Method to get codes
+    public String getCodes(byte symbol){
+        return codingTable.get(symbol);
+    }
+
+    // Method to encode
+    public void enCode(byte[] fileData){
+        // Empty String builder to hold the lists
+
 
     }
 
