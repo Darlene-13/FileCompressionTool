2738bcc323277e19a7f2a4abdea83cc04383b315
Darlene-13 <darlenenasimiyu@gmail.com>
2025-10-28 10:28:00 +0300

    HuffmanDecoder:Initialize an empty byte list and populates it

--- src/main/java/io/github/darlene/algorithms/huffman/HuffmanDecoder.java
+++ src/main/java/io/github/darlene/algorithms/huffman/HuffmanDecoder.java
@@ -1,12 +1,46 @@
 package io.github.darlene.algorithms.huffman;
 
 
-import java.util.HashMap;
-import java.util.Map;
 
-import io.github.darlene.algorithms.huffman.HuffmanTree;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HuffmanDecoder {
+    private HuffmanNode root;
+    private String encodedData;
+    private List<Byte> decodedData;
 
-    Map<String, Integer> reverseMap;
+    // Constructor
+    public HuffmanDecoder(HuffmanTree tree, String encodedData, List<Byte> decodedData){
+        this.root = tree.getRoot();
+        this.encodedData = encodedData;
+        this.decodedData = new ArrayList<>();
+    }
+
+    public void decode(){
+        // Empty list to store decoded bytes
+        HuffmanNode currentNode = root;
+        // For each bit in encoded data
+        for (char bit: encodedData.toCharArray()){
+            if (bit == '0'){
+                currentNode = currentNode.getLeft();
+            } else {
+                if(bit == '1'){
+                    currentNode = currentNode.getRight();
+                }
+            }
+            // Check if we have reached the leaf
+            if(currentNode.isLeaf()){
+                this.decodedData.add(currentNode.getSymbol()); // Populating the decodedData arrayList
+                currentNode = root; // Reset to root for next byte
+            }
+
+        }
+
+    }
+
+    public List<Byte> getDecodedData(List<Byte> li){
+        return this.decodedData;
+    }
+
 }
