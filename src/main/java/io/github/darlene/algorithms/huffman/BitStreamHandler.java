12d776b0434c350a2bc8a8fa7b37a4c6172ed2fc
Darlene-13 <darlenenasimiyu@gmail.com>
2025-10-28 11:54:33 +0300

    Feat:BitStreamHandler to handle bit level operations with string to bit array and viceversa

--- src/main/java/io/github/darlene/algorithms/huffman/BitStreamHandler.java
+++ src/main/java/io/github/darlene/algorithms/huffman/BitStreamHandler.java
@@ -1,4 +1,26 @@
 package io.github.darlene.algorithms.huffman;
 
+import java.util.List;
+
+// Class to handle bit-level operations
+// The encoder produces a string output in 0s and 1s but files store bytes not strings
 public class BitStreamHandler {
+
+    List<Byte> bytes;
+    String bitString;
+
+
+    // String to bit array
+    public void stringToBitArray(String bitString){
+
+    }
+
+    public void bitArrayToString(byte[] data){
+
+    }
+
+    public void getPaddingInfo(){
+
+    }
+
 }
