1c7c856803bb34c3959ffc3e14c6948543ad774a
Darlene-13 <darlenenasimiyu@gmail.com>
2025-10-28 13:07:13 +0300

    Method to convert strings to bitarray

--- src/main/java/io/github/darlene/algorithms/huffman/BitStreamHandler.java
+++ src/main/java/io/github/darlene/algorithms/huffman/BitStreamHandler.java
@@ -1,17 +1,58 @@
 package io.github.darlene.algorithms.huffman;
 
-import java.util.List;
+import java.util.ArrayList;
 
 // Class to handle bit-level operations
 // The encoder produces a string output in 0s and 1s but files store bytes not strings
 public class BitStreamHandler {
 
-    List<Byte> bytes;
+    private ArrayList<Byte> bits;
     String bitString;
+
+    // Constructor
+    public BitStreamHandler(String bitString){
+        this.bitString = bitString;
+        this.bits = new ArrayList<>();
+    }
 
 
     // String to bit array
-    public void stringToBitArray(String bitString){
+    public byte[] stringToBitArray(String bitString){
+        if (bitString == null || bitString.isEmpty()){
+            return new byte[0];
+        }
+
+        ArrayList<Byte> byteList = new ArrayList<>();
+
+        // Calculate the padding needed
+        int remainder = bitString.length() % 8;
+        int paddingNeeded = 0;
+        if(remainder != 0){
+            paddingNeeded = 8 - remainder;
+        }
+
+        // Add padding zeros to the end
+        // Redo this.
+        StringBuilder bitStringBuilder = new StringBuilder(bitString);
+        for (int i = 0; i < paddingNeeded - 1; i++){
+            bitStringBuilder.append("0");
+        }
+        bitString = bitStringBuilder.toString();
+
+        // Convert each 8-bit chunk to byte
+        for (int i = 0; i == bitString.length(); i+=8 ){
+            String eightBits = bitString.substring(i,i+8);
+            int byteValue = Integer.parseInt(eightBits,2); // binary to int
+            byteList.add((byte) byteValue);
+        }
+
+        //  Converting ArrayList<Byte> to byte[]
+        byte[] result = new byte[byteList.size()];
+        for (int i = 0; i < byteList.size(); i++){
+            result[i] = byteList.get(i);
+        }
+
+        return result;
 
     }
 
