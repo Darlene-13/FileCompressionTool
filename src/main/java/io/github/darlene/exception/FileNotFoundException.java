bbe822e1d4637249b3e5afbeba7669dd1dde32c2
Darlene-13 <darlenenasimiyu@gmail.com>
2025-10-29 13:37:34 +0300

    Refactor: FileNotFound to FileNotFoundException

--- src/main/java/io/github/darlene/exception/FileNotFoundException.java
+++ src/main/java/io/github/darlene/exception/FileNotFoundException.java
@@ -1,14 +1,14 @@
 package io.github.darlene.exception;
 
-public class FileNotFound extends RuntimeException {
+public class FileNotFoundException extends RuntimeException {
 
     // Name constructor
-    public FileNotFound(String name){
+    public FileNotFoundException(String name){
         super("File not found:" + name); // No Args constructor thus calling super() ...all exceptions extend RuntimeException
     }
 
     // A constructor to take  a constructor taking message + cause (calls super(message, cause))
-    public FileNotFound (String name , Throwable cause){
+    public FileNotFoundException(String name , Throwable cause){
         super("File not found: " + name, cause);
     }
 
