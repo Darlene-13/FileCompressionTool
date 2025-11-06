a02bdf7b0b70dac02cffd7a75368d1704edad57a
Darlene-13 <darlenenasimiyu@gmail.com>
2025-11-06 13:18:34 +0300

    Modified the exception message

--- src/main/java/io/github/darlene/exception/FormatNotSupportedException.java
+++ src/main/java/io/github/darlene/exception/FormatNotSupportedException.java
@@ -2,9 +2,9 @@ package io.github.darlene.exception;
 
 public class FormatNotSupportedException extends RuntimeException {
     public FormatNotSupportedException(String name){
-        super("The file is corrupted: " + name);
+        super("The file format is not supported: " + name);
     }
     public FormatNotSupportedException(String name, Throwable cause){
-        super("The file is corrupted: "+ name, cause);
+        super("The file format is not suppoted: "+ name, cause);
     }
 }
