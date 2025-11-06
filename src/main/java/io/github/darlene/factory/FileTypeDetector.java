9f3d8e3feea5c19943108808e4f305632cfbb414
Darlene-13 <darlenenasimiyu@gmail.com>
2025-11-06 15:50:41 +0300

    Fixed return type

--- src/main/java/io/github/darlene/factory/FileTypeDetector.java
+++ src/main/java/io/github/darlene/factory/FileTypeDetector.java
@@ -4,15 +4,14 @@ import io.github.darlene.core.CompressionFormat;
 
 public class FileTypeDetector{
 
-    public static detectFormat(String filename){
+    public static CompressionFormat detectFormat(String filename){
         // Get the extension, last part after the dot
         int lastDotIndex = filename.lastIndexOf(".");
         if (lastDotIndex == -1){
             throw new FormatNotSupportedException(" No file extension for the file : " + filename);
         }
         // Extract the extension
-        String extension = filename.substring(lastDotIndex + 1);
-        return extension.upperCase();
+        String extension = "." + filename.substring(lastDotIndex + 1);
 
         // Use compression format enum method
         try{
